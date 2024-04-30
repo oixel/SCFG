a3f76d0154bdc276011a68c31ae04acfaaa3161b
oickle <oycell@aol.com>
2024-04-30 12:00:12 -0400

    Comment out code

--- Scripts/Totems/Totem.gd
+++ Scripts/Totems/Totem.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
+# Gets player to follow
 @export var player : Node
 
+# Shows all potential totem options as a drop down
 @export_enum (
 	"Bird", 
 	"Frog", 
@@ -9,21 +11,33 @@ extends CharacterBody2D
 	"Turtle"
 	) var totem_name : String
 
+# Stores point where totem should fly to
 var totem_point : Node
+
+# Handles totem's special ability
 var ability_handler
 
+# Changes how fast totem moves to totem point
 var fly_speed = 300
-var velo = Vector2(0, 0)
 
 # Creates ability handler for totem's custom ability
 func set_ability():
+	# Changes totem's sprite to look like correct totem
 	$TotemSprite.texture = load("res://Sprites/Totems/" + totem_name + ".png")
+	
+	# Creates a new node under totem and gives it the proper script
 	ability_handler = Node2D.new()
 	ability_handler.name = "AbilityHandler"
 	ability_handler.set_script(load("res://Scripts/Totems/Abilities/" + totem_name + ".gd"))
+	
+	# Initializes which player to grant ability to
 	ability_handler.set_player(player)
+	
+	# Allows ability handler to start running on every frame
 	ability_handler.set_process(true)
 	ability_handler.set_physics_process(true)
+	
+	# Appends ability handler to totem!
 	add_child(ability_handler)
 
 # Intializes totem point and totem's sprite
@@ -38,6 +52,7 @@ func _process(_delta):
 	if is_instance_valid(totem_point):
 		var target_position = (totem_point.global_position - global_position).normalized()
 		
+		# Tracks how far totem is from totem point
 		var gap = global_position.distance_to(totem_point.global_position)
 		
 		# Only move if there is a significant gap between totem and point
