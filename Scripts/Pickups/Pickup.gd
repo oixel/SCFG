6f4ddcb881d2652c091a79bff261a6eceaa24a51
oickle <oycell@aol.com>
2024-05-09 19:05:20 -0400

    Change get_name to get_path

--- Scripts/Pickups/Pickup.gd
+++ Scripts/Pickups/Pickup.gd
@@ -1,10 +1,22 @@
 extends RigidBody2D
 
+@export var sprite : Sprite2D
+
+# Shows all potential pickup types as a drop down
+@export_enum (
+	"Weapon", 
+	"Item",
+	) var type : String
+
 @export var pickup_name: String
 
+
+func _ready():
+	sprite.texture = load("res://Sprites/Pickups/" + get_pickup_path() + ".png")
+
 # Returns the name of this pickup
-func get_pickup_name():
-	return pickup_name.to_lower()
+func get_pickup_path():
+	return type + "s/" + pickup_name.to_lower()
 
 # Runs any final cleanup code before destroying self
 func destroy():
