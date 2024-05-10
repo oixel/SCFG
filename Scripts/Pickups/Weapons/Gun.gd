5602c3d0694f1fe4f579307c91585b9f7bfc3fcc
oickle <oycell@aol.com>
2024-05-10 15:32:08 -0400

    Implement shooting functionality

--- Scripts/Pickups/Weapons/Gun.gd
+++ Scripts/Pickups/Weapons/Gun.gd
@@ -2,10 +2,18 @@ extends Node2D
 
 var player
 
+const BULLET = preload("res://Scenes/Projectiles/bullet.tscn")
+
 # Sets player that holds this pickup
 func set_player(_player):
 	player = _player
 
 # Handles attack when pickup is in hand
 func attack():
-	print("Pew pew!")
+	# Instantiates bullet at player's projectile point
+	var bullet = BULLET.instantiate()
+	player.signal_handler.add_child(bullet)
+	bullet.global_position = player.projectile_point.global_position
+	
+	# Sets movement direction of bullet depending on which way the player is facing
+	bullet.set_direction(sign(player.transform.x.x))
