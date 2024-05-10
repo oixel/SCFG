c8363037968e9cccd596c3c37f787d676b609978
oickle <oycell@aol.com>
2024-05-10 15:34:01 -0400

    Move BASEPICKUP script to scripts

---
+++ Scripts/Pickups/BASEPICKUP.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var player
+
+# Sets player that holds this pickup
+func set_player(_player):
+	player = _player
+
+# Handles attack when pickup is in hand
+func attack():
+	print("Write attack here")
