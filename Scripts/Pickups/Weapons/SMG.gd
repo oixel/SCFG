9325d55a7536b15c087aac83c3ce08b38f335462
oickle <oycell@aol.com>
2024-05-12 13:47:19 -0400

    Implement 3D Switch!

--- Scripts/Pickups/Weapons/SMG.gd
+++ Scripts/Pickups/Weapons/SMG.gd
@@ -4,8 +4,12 @@ extends Node2D
 @export var max_ammo_count : int = 12
 var ammo_count : int
 
+# Handles gap between each bullet shot by SMG
+@export var gap_time : float = 0.025
+var gap_timer : Timer = Timer.new()
+
 # Stores time that it takes for gun to automatically reload
-@export var reload_time : float = 1.5
+@export var reload_time : float = 2
 var reload_timer : Timer = Timer.new()
 
 # Stores player carrying gun to detect when attack is called
@@ -16,22 +20,49 @@ const BULLET = preload("res://Scenes/Projectiles/bullet.tscn")
 
 # Called at start
 func _ready():
-	# Handles reload time
+	# Initializes timer for gaps between each bullet
+	gap_timer.one_shot = true
+	gap_timer.wait_time = gap_time
+	add_child(gap_timer)
+	
+	# Initializes timer for reloading gun
 	reload_timer.one_shot = true
 	reload_timer.wait_time = reload_time
 	add_child(reload_timer)
 	
-	# Sets current ammo count to the max for this gun
-	ammo_count = max_ammo_count
+	# Sets current ammo count to zero to prevent shooting on pickup
+	ammo_count = 0
 
 # Sets player that holds this pickup
 func set_player(_player):
 	player = _player
 
+# Summons bullet and subtracts from ammo count
+func shoot():
+	# Subtracts one bullet from current count
+	ammo_count -= 1
+	
+	# Instantiates bullet at player's projectile point
+	var bullet = BULLET.instantiate()
+	player.signal_handler.add_child(bullet)
+	bullet.global_position = player.projectile_point.global_position
+	
+	# Sets movement direction of bullet depending on which way the player is facing
+	bullet.set_direction(sign(player.transform.x.x))
+
 # Handles actual attacking for SMG
-func _physics_process(delta):
-	if Input.is_action_pressed(player.p_string + "attack"):
-		print("Running")
+func _physics_process(_delta):
+	# Only shoots gun when it has ammo and is not currently reloading
+	if Input.is_action_pressed(player.p_string + "attack") and reload_timer.is_stopped():
+		# Waits a short time before firing each bullet to prevent clumping
+		if gap_timer.is_stopped():
+			if ammo_count > 0:
+				shoot()
+				gap_timer.start()
+	# Reloads gun when out of ammo
+	elif ammo_count <= 0:
+		ammo_count = max_ammo_count
+		reload_timer.start()
 
 # Ensures attack fuction exists for call in player's script but does nothing
 func attack():
