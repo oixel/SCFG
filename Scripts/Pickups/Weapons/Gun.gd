5a3206c29226b2e5453e2e6a8a65904bb7a4f1cc
oickle <oycell@aol.com>
2024-05-11 22:17:38 -0400

    Implement max bullet count and reloading

--- Scripts/Pickups/Weapons/Gun.gd
+++ Scripts/Pickups/Weapons/Gun.gd
@@ -1,8 +1,28 @@
 extends Node2D
 
+# Stores max amount of bullets able to be shot before having to reload
+@export var max_ammo_count : int = 6
+var ammo_count : int
+
+# Stores time that it takes for gun to automatically reload
+@export var reload_time : float = 1.5
+var reload_timer : Timer = Timer.new()
+
+# Stores player carrying gun to detect when attack is called
 var player
 
+# Preloads bullet scene for instantiating when shooting
 const BULLET = preload("res://Scenes/Projectiles/bullet.tscn")
+
+# Called at start
+func _ready():
+	# Handles reload time
+	reload_timer.one_shot = true
+	reload_timer.wait_time = reload_time
+	add_child(reload_timer)
+	
+	# Sets current ammo count to the max for this gun
+	ammo_count = max_ammo_count
 
 # Sets player that holds this pickup
 func set_player(_player):
@@ -10,10 +30,21 @@ func set_player(_player):
 
 # Handles attack when pickup is in hand
 func attack():
-	# Instantiates bullet at player's projectile point
-	var bullet = BULLET.instantiate()
-	player.signal_handler.add_child(bullet)
-	bullet.global_position = player.projectile_point.global_position
+	# Only allows shooting if ammo available and not reloading
+	if ammo_count > 0 and reload_timer.is_stopped():
+		
+		# Subtracts one bullet from current count
+		ammo_count -= 1
+		
+		# Instantiates bullet at player's projectile point
+		var bullet = BULLET.instantiate()
+		player.signal_handler.add_child(bullet)
+		bullet.global_position = player.projectile_point.global_position
+		
+		# Sets movement direction of bullet depending on which way the player is facing
+		bullet.set_direction(sign(player.transform.x.x))
 	
-	# Sets movement direction of bullet depending on which way the player is facing
-	bullet.set_direction(sign(player.transform.x.x))
+	# Automatically starts reload when out of bullets
+	if ammo_count <= 0:
+		ammo_count = max_ammo_count
+		reload_timer.start()
