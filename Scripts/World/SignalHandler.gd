3409ed9e74e7e8c67e3f343c810b556f0efea16c
oickle <oycell@aol.com>
2024-05-17 17:07:56 -0400

    Change reset key to ESC to avoid conflict with aim right key

--- Scripts/World/SignalHandler.gd
+++ Scripts/World/SignalHandler.gd
@@ -6,5 +6,5 @@ signal alter_portrait(p_string : String, face_string : String)
 # FOR DEBUG PURPOSES ONLY
 func _physics_process(_delta):
 	# Resets scene when "R" is pressed
-	if (Input.is_key_pressed(KEY_R)):
+	if (Input.is_key_pressed(KEY_ESCAPE)):
 		get_tree().reload_current_scene()
