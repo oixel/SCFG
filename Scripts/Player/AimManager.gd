2ac668d3656a32337917cba2e82e7ed78e326531
oickle <oycell@aol.com>
2024-05-20 10:16:39 -0400

    Clean code and tweak values

--- Scripts/Player/AimManager.gd
+++ Scripts/Player/AimManager.gd
@@ -3,7 +3,12 @@ extends Node2D
 var p_string
 
 @export var sprite : Sprite2D
+
+# Used by weapons that need aiming to shoot projectiles in correct direction
 var aim_rotation
+
+# Value by which the aim line is rotated every frame of input
+@export var rotation_speed : float = 0.1
 
 # Timer that hides the aim line after a certain amount of time passes after aiming
 @export var time_before_hide : float = 3
@@ -15,41 +20,39 @@ func set_p_string(_p_string):
 
 # Called at start
 func _ready():
+	# Ensures that aim line is not visible at start
+	sprite.hide()
+	
 	# Initializes timer for hiding the aim line after aiming
 	hide_timer.one_shot = true
 	hide_timer.wait_time = time_before_hide
 	add_child(hide_timer)
 
+# Rotates aim line depending on multiplier (-1 or 1)
+func rotate_aim(multiplier, out_of_range) -> void:
+	sprite.rotation += rotation_speed * multiplier
+	
+	# Ensures rotation doesn't go out of range of -360 < x < 360
+	if out_of_range:
+		sprite.rotation = 0
+	
+	# Ensures that aim line is visible when aiming
+	if !sprite.visible:
+		sprite.show()
+		hide_timer.start()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	#
-	# VERY ROUGH TEST FOR AIMING
-	#
+	# Rotates the aim line clockwise
 	if Input.is_action_pressed(p_string + "aim_right"):
-		sprite.rotation += 0.05
-		if sprite.rotation > 360:
-			sprite.rotation = 0
-		
-		# Ensures that aim line is visible when aiming
-		if !sprite.visible:
-			sprite.show()
-			hide_timer.start()
+		rotate_aim(1, sprite.rotation > 360)
 	
+	# Rotates the aim line counter-clockwise
 	if Input.is_action_pressed(p_string + "aim_left"):
-		sprite.rotation -= 0.05
-		if sprite.rotation < -360:
-			sprite.rotation = 0
-		
-		# Ensures that aim line is visible when aiming
-		if !sprite.visible:
-			sprite.show()
-			hide_timer.start()
+		rotate_aim(-1, sprite.rotation < -360)
 	
+	# Updates the aim_rotation variable to match the aim line
 	aim_rotation = sprite.rotation
-	
-	#
-	#
-	#
 	
 	# Hides aim line if time has passed
 	if hide_timer.is_stopped() and sprite.visible:
