e74b14aad2de8e017292535b4f75d90c009c5126
oickle <oycell@aol.com>
2024-05-20 12:06:10 -0400

    Add basic teleporting functionality

--- Scripts/Misc/Hole.gd
+++ Scripts/Misc/Hole.gd
@@ -7,15 +7,22 @@ extends Node2D
 var exit_point
 
 # Keep an array of things just passed through to prevent infinite teleporting
+var need_to_exit := []
 
 # Called in mole script to set exit point
 func set_exit(exit):
 	exit_point = exit
 
-# 
+# Moves object passed in to out point
 func teleport(obj):
+	need_to_exit.append(obj)
 	obj.global_position = out.global_position
 
-func _on_area_2d_body_entered(obj):
+# Checks every frame if a player is trying to use hole
+func _physics_process(_delta):
 	if exit_point:
-		exit_point.teleport(obj)
+		if area.get_overlapping_bodies():
+			for obj in area.get_overlapping_bodies():
+				if obj.is_in_group("Player"):
+					if Input.is_action_just_pressed(obj.p_string + "down"):
+						exit_point.teleport(obj)
