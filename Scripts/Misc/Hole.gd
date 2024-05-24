be0cabf5ac031527552b72d15ecf8db97925c06b
oickle <oycell@aol.com>
2024-05-24 12:00:36 -0400

    Add debug prints

--- Scripts/Misc/Hole.gd
+++ Scripts/Misc/Hole.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @export var area : Area2D
-@export var out : Marker2D
+@export var out : Node2D
 
 # Set in mole script;
 var exit_point
@@ -12,7 +12,12 @@ func set_exit(exit):
 
 # Moves object passed in to out point
 func teleport(obj):
-	obj.global_position = out.global_position
+	obj.global_position.x = out.global_position.x
+	obj.global_position.y = out.global_position.y
+	print(str(obj.global_position.y) + " :: Player")
+	print(str(exit_point.out.global_position.y) + " :: Exit")
+	print(str(out.global_position.y) + " :: Enter")
+	print()
 
 # Checks every frame if a player is trying to use hole
 func _physics_process(_delta):
