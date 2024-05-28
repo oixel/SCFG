c219e249ea09c2987fc773a3f20025053b86a662
oickle <oycell@aol.com>
2024-05-28 14:19:51 -0400

    Add support for digging into walls

--- Scripts/Totems/Abilities/Mole.gd
+++ Scripts/Totems/Abilities/Mole.gd
@@ -12,29 +12,49 @@ var button_tapped : String
 var hole1 = null
 var hole2 = null
 
+enum state {LEFT, RIGHT, DOWN, UP}
+
 func set_player(_player : CharacterBody2D):
 	player = _player
 	p_string = player.p_string
 
+# 
+func create_hole(hole_state) -> Node2D:
+	var offset = Vector2(0, 0)
+	var key
+	if hole_state == state.DOWN:
+		offset = Vector2(0, 55)
+		key = "down"
+	elif hole_state == state.LEFT:
+		offset = Vector2(-55, 0)
+		key = "left"
+	elif hole_state == state.RIGHT:
+		offset = Vector2(55, 0)
+		key = "right"
+		
+	var hole = HOLE.instantiate()
+	hole.global_position = player.global_position
+	hole.sprite.global_position += offset
+	
+	hole.set_key(key)
+	
+	player.signal_handler.add_child(hole)
+	return hole
+
 #
-func dig() -> void:
+func dig(hole_state) -> void:
 	if !hole1:
-		hole1 = HOLE.instantiate()
-		hole1.global_position = player.global_position
-		player.signal_handler.add_child(hole1)
+		hole1 = create_hole(hole_state)
 	elif !hole2:
-		hole2 = HOLE.instantiate()
-		hole2.global_position = player.global_position
-		player.signal_handler.add_child(hole2)
+		hole2 = create_hole(hole_state)
+		
 		hole1.set_exit(hole2)
 		hole2.set_exit(hole1)
 	else:
 		hole1.queue_free()
 		hole1 = hole2
 		
-		hole2 = HOLE.instantiate()
-		hole2.global_position = player.global_position
-		player.signal_handler.add_child(hole2)
+		hole2 = create_hole(hole_state)
 		
 		hole1.set_exit(hole2)
 		hole2.set_exit(hole1)
@@ -55,8 +75,26 @@ func _physics_process(_delta):
 	if Input.is_action_just_pressed(p_string + "down"):
 		if !double_tap_timer.is_stopped() and button_tapped == p_string + "down":
 			if player.is_on_floor() and !player.rolling:
-				dig()
+				dig(state.DOWN)
 				double_tap_timer.stop()
 		else:
 			button_tapped = p_string + "down"
+			double_tap_timer.start()
+	
+	if Input.is_action_just_pressed(p_string + "left"):
+		if !double_tap_timer.is_stopped() and button_tapped == p_string + "left":
+			if player.is_on_wall():
+				dig(state.LEFT)
+				double_tap_timer.stop()
+		else:
+			button_tapped = p_string + "left"
+			double_tap_timer.start()
+			
+	if Input.is_action_just_pressed(p_string + "right"):
+		if !double_tap_timer.is_stopped() and button_tapped == p_string + "right":
+			if player.is_on_wall():
+				dig(state.RIGHT)
+				double_tap_timer.stop()
+		else:
+			button_tapped = p_string + "right"
 			double_tap_timer.start()
