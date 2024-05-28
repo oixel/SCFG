6eec0896e64608cccf5d12b800c20b9c406769f4
oickle <oycell@aol.com>
2024-05-28 14:19:39 -0400

    Add specific key detection functionality

--- Scripts/Misc/Hole.gd
+++ Scripts/Misc/Hole.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var area : Area2D
 @export var out : Node2D
+@export var sprite : Sprite2D
+var key : String = "down"
 
 # Set in mole script;
 var exit_point
@@ -9,6 +11,10 @@ var exit_point
 # Called in mole script to set exit point
 func set_exit(exit):
 	exit_point = exit
+
+# 
+func set_key(_key : String):
+	key = _key
 
 # Moves object passed in to out point
 func teleport(obj):
@@ -20,5 +26,5 @@ func _physics_process(_delta):
 		if area.get_overlapping_bodies():
 			for obj in area.get_overlapping_bodies():
 				if obj.is_in_group("Player"):
-					if Input.is_action_just_pressed(obj.p_string + "down"):
+					if Input.is_action_just_pressed(obj.p_string + key):
 						exit_point.teleport(obj)
