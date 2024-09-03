62771d654f9f2e481232451422ccf4a52efacdb9
oickle <oycell@aol.com>
2024-09-03 19:32:23 -0400

    Add proper pause input map

--- Scripts/World/SignalHandler.gd
+++ Scripts/World/SignalHandler.gd
@@ -6,5 +6,5 @@ signal alter_portrait(p_string : String, face_string : String)
 # FOR DEBUG PURPOSES ONLY
 func _physics_process(_delta):
 	# Resets scene when "R" is pressed
-	if (Input.is_key_pressed(KEY_ESCAPE)):
+	if (Input.is_action_just_pressed("keyboard_pause") or Input.is_action_just_pressed("controller_pause")):
 		get_tree().reload_current_scene()
