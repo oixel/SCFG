fa7526dad99e7636f37fedf326c84e5f39b50977
oickle <oycell@aol.com>
2024-09-03 16:27:43 -0400

    Change Hole to work based on interact button

--- Scripts/Misc/Hole.gd
+++ Scripts/Misc/Hole.gd
@@ -3,7 +3,6 @@ extends Node2D
 @export var area : Area2D
 @export var out : Node2D
 @export var sprite : Sprite2D
-var key : String = "down"
 
 # Set in mole script;
 var exit_point
@@ -11,10 +10,6 @@ var exit_point
 # Called in mole script to set exit point
 func set_exit(exit):
 	exit_point = exit
-
-# 
-func set_key(_key : String):
-	key = _key
 
 # Moves object passed in to out point
 func teleport(obj):
@@ -26,5 +21,5 @@ func _physics_process(_delta):
 		if area.get_overlapping_bodies():
 			for obj in area.get_overlapping_bodies():
 				if obj.is_in_group("Player"):
-					if Input.is_action_just_pressed(obj.p_string + key):
+					if Input.is_action_just_pressed(obj.p_string + "interact"):
 						exit_point.teleport(obj)
