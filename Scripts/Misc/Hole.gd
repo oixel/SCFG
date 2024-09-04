76c5164ddcd51f44a2aa8e832f9487d4d0907bc2
oickle <oycell@aol.com>
2024-09-04 12:18:22 -0400

    Tweak how entering holes works

--- Scripts/Misc/Hole.gd
+++ Scripts/Misc/Hole.gd
@@ -17,10 +17,21 @@ func teleport(obj):
 
 # Checks every frame if a player is trying to use hole
 func _physics_process(_delta):
+	# Only allows players to use hole if not attempting to parry and not currently rolling
 	if exit_point:
 		if area.get_overlapping_bodies():
 			for obj in area.get_overlapping_bodies():
 				if obj.is_in_group("Player"):
-					if !obj.rolling:
+					if !obj.rolling and !obj.crouched:
 						if Input.is_action_just_pressed("%s_roll" % obj.control_type):
 							exit_point.teleport(obj)
+
+# Prevents player from rolling while on the enterance of a hole
+func _on_area_2d_body_entered(target):
+	if target.is_in_group("Player"):
+		target.can_roll = false
+
+# Allows player to roll again after exiting hole enterance
+func _on_area_2d_body_exited(target):
+	if target.is_in_group("Player"):
+		target.can_roll = true
