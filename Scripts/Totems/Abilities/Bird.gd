b98f94ce24530e8e25b1c17a0c5f6e1c2640d99f
oickle <oycell@aol.com>
2024-09-04 23:15:37 -0400

    Remove unused variable declaration

--- Scripts/Totems/Abilities/Bird.gd
+++ Scripts/Totems/Abilities/Bird.gd
@@ -22,8 +22,6 @@ func set_player(_player : CharacterBody2D):
 	player = _player
 	control_type = player.control_type
 	normal_weight = player.weight
-	
-	player.can_air_roll = false
 
 func _physics_process(delta):
 	# Prevents code from running if player just died
